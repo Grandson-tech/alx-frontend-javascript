f8a88bde1e180127333ab0ca5c66fae95e267ad7
Dylan Tirike <tirikedylan59@gmail.com>
2025-10-03 01:38:31 +0300

    Fix TypeScript project issues: add missing .eslintrc.js and fix namespace imports

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -1,17 +1,19 @@
-import { Subjects } from './subjects/Teacher';
-import { Subjects as SubjectNamespace } from './subjects/Subject';
-import { Subjects as CppNamespace } from './subjects/Cpp';
-import { Subjects as ReactNamespace } from './subjects/React';
-import { Subjects as JavaNamespace } from './subjects/Java';
+/// <reference path="./subjects/Teacher.ts" />
+/// <reference path="./subjects/Subject.ts" />
+/// <reference path="./subjects/Cpp.ts" />
+/// <reference path="./subjects/React.ts" />
+/// <reference path="./subjects/Java.ts" />
 
-export const cpp = new CppNamespace.Cpp();
-export const java = new JavaNamespace.Java();
-export const react = new ReactNamespace.React();
+export const cpp = new Subjects.Cpp();
+export const java = new Subjects.Java();
+export const react = new Subjects.React();
 
 export const cTeacher: Subjects.Teacher = {
   firstName: 'John',
   lastName: 'Doe',
-  experienceTeachingC: 10
+  experienceTeachingC: 10,
+  experienceTeachingReact: 10,
+  experienceTeachingJava: 10
 };
 
 console.log('C++');
